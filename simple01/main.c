93f3352ee6782fee7b4ae240819951867fff1abe
christian-dale <jchaukvik@gmail.com>
2026-01-17 19:53:16 +0100

    simple01: Improve readability of code

--- simple01/main.c
+++ simple01/main.c
@@ -7,15 +7,16 @@ double linear_model(double weight, double bias, double x) {
 int main(int argc, char *argv[]) {
     double training_data_x[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
     double training_data_y[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+    size_t data_length = 10;
 
     double weight = 0.0;
     double bias = 0.0;
     double learning_rate = 0.01;
 
     for (int epoch = 0; epoch < 10000; epoch++) {
-        for (int i = 0; i < 10; i++) {
-            double x = training_data_x[i] / 10.0;
-            double y_true = training_data_y[i] / 10.0;
+        for (int i = 0; i < data_length; i++) {
+            double x = training_data_x[i] / (double) data_length;
+            double y_true = training_data_y[i] / (double) data_length;
             double y_pred = weight * x + bias;
             double error = y_pred - y_true;
 
@@ -30,5 +31,8 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    printf("Model test: input = %f, output = %f\n", 5.2, linear_model(weight, bias, 5.2 / 10.0) * 10.0);
+    printf(
+        "Model test: input = %f, output = %f\n", 5.2,
+        linear_model(weight,bias, 5.2 / (double) data_length) * (double) data_length
+    );
 }
